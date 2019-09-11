031b06f94b463716ef444401eb2a4546323da49b
= <=>
2019-09-10 22:10:38 -0300

    implementando a leitura das tabelas

--- condutores.py
+++ condutores.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 
 def tabela(numero_tabela, sheet = False):
-    excel_file = pd.read_excel(f"Tabelas\Tabela {numero_tabela}.xlsx")
+    excel_file = pd.ExcelFile(f"Tabelas\Tabela {numero_tabela}.xlsx")
+
+    sheet_names = excel_file.sheet_names
+    if (len(sheet_names) == 1):
+        return excel_file.sheet_names[0]
+
+
+table = tabela(4.1)
     
-    return excel_file
 
-
-    
-
+print(table.head())
