bd3603476879eb7d0b0d926c8eab0a17655deff4
Michael Henrique <oliveirasantos.mh@gmail.com>
2019-09-11 09:10:02 -0300

    Função Tabela Finalizada

--- condutores.py
+++ condutores.py
@@ -1,13 +1,10 @@
 import pandas as pd 
 
-<<<<<<< HEAD
-=======
 # Função para ler uma planilha Excel
 def tabela(numero_tabela, sheet_name = "Sheet1"):
     """ ----- Função para ler um arquivo do Excel e suas planilhas -----
         Primeiro Argumento é o número da Tabela referente ao livro do Mamede 4ª Ed
         Segundo Argumento é uma string com o nome da planilha"""
->>>>>>> NewFeature
 
 def tabela(numero_tabela, sheet = False):
     excel_file = pd.read_excel(f"Tabelas\Tabela {numero_tabela}.xlsx")
