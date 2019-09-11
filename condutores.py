a7d9b94e95cbc67231a8ee73cedd4771437b8d5d
Michael Henrique <oliveirasantos.mh@gmail.com>
2019-09-11 08:53:17 -0300

    Função para ler as planilhas

--- condutores.py
+++ condutores.py
@@ -1,15 +1,16 @@
 import pandas as pd 
 
+# Função para ler uma planilha Excel
+def tabela(numero_tabela, sheet_name = "Sheet1"):
+    """ ----- Função para ler um arquivo do Excel e suas planilhas -----
+        Primeiro Argumento é o número da Tabela referente ao livro do Mamede 4ª Ed
+        Segundo Argumento é o nome da planilha"""
 
-def tabela(numero_tabela, sheet = False):
-    excel_file = pd.ExcelFile(f"Tabelas\Tabela {numero_tabela}.xlsx")
+    try:
+        # Variável que armazena a planilha em formato Pandas    
+        sheet = pd.read_excel(f"Tabelas\Tabela {numero_tabela}.xlsx", sheet_name = sheet_name)
+    except:
+        # Sai do programa caso o nome da planilha esteja errado
+        exit(f"Não existe planilha com o nome {sheet_name}")
 
-    sheet_names = excel_file.sheet_names
-    if (len(sheet_names) == 1):
-        return excel_file.sheet_names[0]
-
-
-table = tabela(4.1)
-    
-
-print(table.head())
+    return sheet
