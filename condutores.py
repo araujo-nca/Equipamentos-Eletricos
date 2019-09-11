b06b527aaa980d5f8442311cbeb2e8521d8ee1ca
= <=>
2019-09-11 09:14:08 -0300

    Função Tabela Finalizada

--- condutores.py
+++ condutores.py
@@ -6,7 +6,11 @@ def tabela(numero_tabela, sheet_name = "Sheet1"):
         Primeiro Argumento é o número da Tabela referente ao livro do Mamede 4ª Ed
         Segundo Argumento é uma string com o nome da planilha"""
 
-def tabela(numero_tabela, sheet = False):
-    excel_file = pd.read_excel(f"Tabelas\Tabela {numero_tabela}.xlsx")
-    
-    return excel_file
+    try:
+        # Variável que armazena a planilha em formato Pandas    
+        sheet = pd.read_excel(f"Tabelas\Tabela {numero_tabela}.xlsx", sheet_name = sheet_name)
+    except:
+        # Sai do programa caso o nome da planilha esteja errado
+        exit(f"Não existe planilha com o nome {sheet_name}")
+
+    return sheet
