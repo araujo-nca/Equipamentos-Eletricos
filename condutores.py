f210396451bfa21c4a96279bda8f14ec0835d579
Michael Henrique <oliveirasantos.mh@gmail.com>
2019-09-11 08:58:27 -0300

    Função tabela - Ler o arquivo Excel e uma tabela especifica do Arquivo XLSX

--- condutores.py
+++ condutores.py
@@ -4,7 +4,7 @@ import pandas as pd
 def tabela(numero_tabela, sheet_name = "Sheet1"):
     """ ----- Função para ler um arquivo do Excel e suas planilhas -----
         Primeiro Argumento é o número da Tabela referente ao livro do Mamede 4ª Ed
-        Segundo Argumento é o nome da planilha"""
+        Segundo Argumento é uma string com o nome da planilha"""
 
     try:
         # Variável que armazena a planilha em formato Pandas    
