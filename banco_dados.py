01aebb9a4b79beaba9e749c0a50c7dc242aad2a7
= <=>
2019-10-04 18:02:39 -0300

    Adicionado arquivos ao banco de dados

--- banco_dados.py
+++ banco_dados.py
@@ -1,4 +1,4 @@
-from  planilhas import Planilha
+from planilhas import Planilha
 
 tab421 = Planilha("Condutores", "4.21")
 tab421 = tab421.replace({'Cabo isolado em XLPE':'XLPE', 'Cabo isolado em EPR':'EPR'})
@@ -13,4 +13,14 @@ tab622 = Planilha("Transformador de Potencial", "6.2.2")
 tab641 = Planilha("Transformador de Potencial", "6.4.1")
 tab642 = Planilha("Transformador de Potencial", "6.4.2")
 tab66 = Planilha("Transformador de Potencial", "6.6")
+
+tab51 = Planilha("Transformador de Corrente", "5.1")
+tab52 = Planilha("Transformador de Corrente", "5.2")
+tab53 = Planilha("Transformador de Corrente", "5.3")
+tab54 = Planilha("Transformador de Corrente", "5.4")
+tab55 = Planilha("Transformador de Corrente", "5.5")
+tab56 = Planilha("Transformador de Corrente", "5.6")
+tab57 = Planilha("Transformador de Corrente", "5.7")
+
+
 
