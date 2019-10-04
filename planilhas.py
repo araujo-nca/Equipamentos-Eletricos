4dd2d0b53e435398c4c7a200b20cb41ebc4ef838
Michael Henrique <oliveirasantos.mh@gmail.com>
2019-10-04 16:17:09 -0300

    Melhorando na função planilha

--- planilhas.py
+++ planilhas.py
@@ -1,17 +1,39 @@
 import pandas as pd
+import platform
 
-class Planilha():
-    """ ----- Classe para criar a tabela de um arquivo do Excel -----
-        Primeiro Argumento é o número da Tabela referente ao livro do Mamede 4ª Ed
-        Segundo Argumento é uma string com o nome da planilha"""
 
-    def tabela(self, sheet_name):
+sistema = platform.system()
 
-        try:
-            # Variável que armazena a planilha em formato Pandas    
-            sheet = pd.read_excel(f"/home/deilson/EquipamentosEletricos/tabelas.xlsx", sheet_name = sheet_name)
-        except:
-            # Sai do programa caso o nome da planilha esteja errado
-            exit(f"Não existe planilha com o nome {sheet_name}")
 
-        return sheet
+def Planilha(equipamento, tabela):
+    """Função para criar a tabela de um arquivo do Excel
+
+    Parâmetros:
+
+        equipamento: string
+
+            Equipamento que se precisa consultar a tabela
+
+        tabela: string
+
+            Número da Tabela referente ao livro do Mamede 4ª Ed """
+
+    
+    try:
+        # Variável que armazena a planilha em formato Pandas    
+        if(sistema == "Windows"):
+            sheet = pd.read_excel(f"Tabelas/{equipamento}.xlsx", sheet_name = tabela)
+        
+        # Fiz isso pois não sei se o diretório no sistema linux funciona da mesma maneira. Caso tenha erro, mudar essa parte do código.
+        elif(sistema == "Linux"):
+            sheet = pd.read_excel(f"Tabelas/{equipamento}.xlsx", sheet_name = tabela)
+
+        else:
+            exit("Sistema Operacional não reconhecido.")
+
+    except:
+        # Sai do programa caso o nome da planilha esteja errado
+        exit(f"Não existe planilha com o nome {tabela}")
+
+    return sheet
+
